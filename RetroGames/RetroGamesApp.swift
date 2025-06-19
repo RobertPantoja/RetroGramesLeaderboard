125b0fa3cdc6de7e51fc00b95ef1bcdc3bae3181
Roberto Pantoja <74929633+RobertPantoja@users.noreply.github.com>
2025-06-18 20:34:44 -0600

    Initial Commit

---
+++ RetroGames/RetroGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetroGamesApp.swift
+//  RetroGames
+//
+//  Created by Roberto Pantoja on 18/06/25.
+//
+
+import SwiftUI
+
+@main
+struct RetroGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RetroGamesTests/RetroGamesTests.swift
@@ -0,0 +1,17 @@
+//
+//  RetroGamesTests.swift
+//  RetroGamesTests
+//
+//  Created by Roberto Pantoja on 18/06/25.
+//
+
+import Testing
+@testable import RetroGames
+
+struct RetroGamesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
